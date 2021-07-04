11c4155a2016022c4e2549300decde39fd1dc85b
vinaka <k64.yuta29@gmail.com>
2021-07-04 17:12:21 +0900

    ファイル分割

---
+++ src/components/Square.tsx
@@ -0,0 +1,15 @@
+// Square.tsx
+import React from 'react';
+import { ISquare } from '../interface';
+interface SquareProps {
+    value: ISquare;
+    onClick: () => void;
+  }
+  function Square(props: SquareProps) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
+  export default Square;
